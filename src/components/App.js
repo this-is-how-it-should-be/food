0cbe83d1bc77bba3669308dad7d58fae2875ea87
Ricardo Guerra <this.is.how.it.should.b@gmail.com>
2025-04-03 16:12:00 -0400

    Added import statement to import useEffect, used useEffect to manage a side effect function getFoods(), which retrieves foods data using the fetch() method and updates the foods state appropriately using the setFoods() setter function

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import Header from "./Header";
 import FoodMenu from "./FoodMenu";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
 
   const [foods, setFoods] = useState([]);
+
+  useEffect(getFoods, [])
+
+  function getFoods(){
+    fetch("https://raw.githubusercontent.com/this-is-how-it-should-be/food/refs/heads/main/db.json")
+    .then(response => response.json())
+    .then(data => {
+      setFoods(data.foods)
+    })
+  }
 
   return (
     <>
