9d21c697563e30eb5bc9d1c56ece9d7bc890de2e
Ricardo Guerra <this.is.how.it.should.b@gmail.com>
2025-04-03 16:02:34 -0400

    Added import statement to import useState, and set up state for foods data

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import Header from "./Header";
 import FoodMenu from "./FoodMenu";
 
+import { useState } from "react";
+
 function App() {
+
+  const [foods, setFoods] = useState([]);
+
   return (
     <>
       <Header/>
